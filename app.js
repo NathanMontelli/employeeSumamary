e7efd5d33b54467b05bba7c13c13d5bf0a3f24b6
Nathan Montelli <nathanmontelli@gmail.com>
2021-12-14 01:32:11 -0800

    finished addTeamMember function

--- app.js
+++ app.js
@@ -39,48 +39,48 @@ const questions = () => {
       choices: ['Manager', 'Engineer', 'Intern']
     }
   ])
-  .then(data => {
-    if (data.role == 'Manager') {
-      inquirer.prompt([
-        {
-          type: 'input',
-          name: 'officeNumber',
-          message: 'What is your office number?'
-        }])
-    .then(managerData => {
-      const newManager = new Manager(data.name, data.id, data.email, managerData.officeNumber)
-      employeeData.push(newManager)
-      console.log(employeeData)
-      addTeamMember()
+    .then(data => {
+      if (data.role == 'Manager') {
+        inquirer.prompt([
+          {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is your office number?'
+          }])
+          .then(managerData => {
+            const newManager = new Manager(data.name, data.id, data.email, managerData.officeNumber)
+            employeeData.push(newManager)
+            console.log(employeeData)
+            addTeamMember()
+          })
+      } else if (data.role == 'Engineer') {
+        inquirer.prompt([
+          {
+            type: 'input',
+            name: 'github',
+            message: 'What is the name of your Github profile?'
+          }])
+          .then(engineerData => {
+            const newEngineer = new Engineer(data.name, data.id, data.email, engineerData.github)
+            employeeData.push(newEngineer)
+            console.log(employeeData)
+            addTeamMember()
+          })
+      } else if (data.role == 'Intern') {
+        inquirer.prompt([
+          {
+            type: 'input',
+            name: 'school',
+            message: 'What school do you currently attend?'
+          }])
+          .then(internData => {
+            const newIntern = new Intern(data.name, data.id, data.email, internData.school)
+            employeeData.push(newIntern)
+            console.log(employeeData)
+            addTeamMember()
+          })
+      }
     })
-    } else if (data.role == 'Engineer') {
-      inquirer.prompt([
-        {
-          type: 'input',
-          name: 'github',
-          message: 'What is the name of your Github profile?'
-        }])
-    .then(engineerData => {
-      const newEngineer = new Engineer(data.name, data.id, data.email, engineerData.github)
-      employeeData.push(newEngineer)
-      console.log(employeeData)
-      addTeamMember()
-    })
-    } else if (data.role == 'Intern') {
-      inquirer.prompt([
-        {
-          type: 'input',
-          name: 'school',
-          message: 'What school do you currently attend?'
-        }])
-    .then(internData => {
-      const newIntern = new Intern(data.name, data.id, data.email, internData.school)
-      employeeData.push(newIntern)
-      console.log(employeeData)
-      addTeamMember()
-    })
-    }
-  })
 }
 
 const addTeamMember = () => {
@@ -90,22 +90,24 @@ const addTeamMember = () => {
     message: 'Would you like to add another team member?',
     choices: ['yes', 'no']
   })
-.then(data => {
-  if(data.addMember == 'yes') {
-  questions()
-  } else (data.addMember == 'no') {
-    
-  }
-})
+    .then(data => {
+      if (data.addMember == 'yes') {
+        questions()
+      } else if (data.addMember == 'no') {
+          fs.mkdir('output', { recursive: true }, (err => {
+            if (err) { console.log(err) }
+          }))
+          writeToFile('./output/team.html', render(employeeData))
+      }
+    })
 }
-
 const writeToFile = (fileName, data) => {
-  fs.writeFile(fileName, data, err => {
-    if (err) {console.log(err)}
-  })
-}
+    fs.writeFile(fileName, data, err => {
+      if (err) { console.log(err) }
+    })
+  }
 
-questions()
+  questions()
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
